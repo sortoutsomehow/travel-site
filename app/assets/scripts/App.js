f7cb5d9ff49e24079432b380283fdcf4f4c2e705
sortoutsomehow <alighaffary4174@gmail.com>
2024-03-11 15:38:05 +0330

    sticky header done

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,9 @@
 import '../styles/style.css'
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
+import StyckyHeader from './modules/StickyHeader'
 
-
+let stickyHeader = new StyckyHeader
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
 let mobileMenu = new MobileMenu()
